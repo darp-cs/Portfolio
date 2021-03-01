b65d4e5ba9bdde7862d953acf12f1f4b7106421c
darp-cs <diegoarod14@gmail.com>
2021-03-01 13:31:07 -0500

    Changed bio to about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import classes from './About.module.css'
 const About = () => {
     return (
         <div className={classes.about}>
-            <h1>Bio</h1>
+            <h1>About Me</h1>
             <p>
            In my academic life I'm currently attending the University of North Carolina at Charlotte working on both my bachelors and masters in computer science, with a  concentration in AI, Gaming, and Robotics.I have a passion for computer science as a whole with a particular interest in AI, specifically deep learning. I strive to improve and refine my skills every day and enjoy giving back to the computer science community. 
             </p>
