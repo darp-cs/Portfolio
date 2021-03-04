529c4e07fbb7b645c4db289ceb48f7edc5a7912a
darp-cs <diegoarod14@gmail.com>
2021-03-04 13:57:18 -0500

    Added correct Comments

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,11 +4,11 @@ const About = () => {
     return (
         <div className={classes.about}>
             <h1>About Me</h1>
-            <p>
+            <p className = {classes.info}>
            In my academic life I'm currently attending the University of North Carolina at Charlotte working on both my bachelors and masters in computer science, with a  concentration in AI, Gaming, and Robotics.I have a passion for computer science as a whole with a particular interest in AI, specifically deep learning. I strive to improve and refine my skills every day and enjoy giving back to the computer science community. 
             </p>
             <br></br>
-            <p>
+            <p  className = {classes.info}>
             In my professional life, I'm currently a part of road to hire as a coding apprentice learning and developing web applications using programming languages such as JavaScript, MySQL along with markup languages. I have done several implementations using the React framework and SASS. In addition I've also had the opportunity to study AWS cloud computing.
             </p>
 
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,7 +13,7 @@ const projects = () =>{
                 <img className= {classes.projectImg} src = "https://raw.githubusercontent.com/darp-cs/Logo_Images/main/hope-hacks-img.png?raw=true" alt= "Hope Hackathon preview"/>
                 <div>
                         <h2 className= {classes.projectInfo}>Hope Hackathon</h2>
-                        <p>This project focused on bringing attention to a problem within our society, in this case the environment, and ways that we can prevent it. This project uses javascript and express for the back end requests.</p>
+                        <p className = {classes.infotext}>This project focused on bringing attention to a problem within our society, in this case the environment, and ways that we can prevent it. This project uses javascript and express for the back end requests.</p>
                 </div>
                 <button className = {classes.projectButton}><a href ="https://github.com/darp-cs/Hope-Hackathon.git" target="_blank">Source Code</a></button>
                 </div>
@@ -24,7 +24,7 @@ const projects = () =>{
                 <img className= {classes.projectImg} src = "https://raw.githubusercontent.com/darp-cs/Logo_Images/main/burgerbuilder-img.png?raw=true" alt= "Burger builder preview"/>
                 <div>
                         <h2 className= {classes.projectInfo}>Burger Builder</h2>
-                        <p>This project demonstrates the use of a sound structure, and classical components. The application itself will allow a user to create a burger with the topings they want and also allow them to remove the toppings. This project was also built with react.</p>
+                        <p className = {classes.infotext}>This project demonstrates the use of a sound structure, and classical components. The application itself will allow a user to create a burger with the topings they want and also allow them to remove the toppings. This project was also built with react.</p>
                 </div>
                 <button className = {classes.projectButton}><a href ="https://github.com/darp-cs/Week-18" target="_blank">Source Code</a></button>
                 </div>
@@ -34,7 +34,7 @@ const projects = () =>{
                 <img className= {classes.projectImg} src = "https://raw.githubusercontent.com/darp-cs/Logo_Images/main/ecommerce-img.png?raw=true" alt= "Ecommerce Preview"/>
                 <div>
                         <h2 className= {classes.projectInfo}>Ecommerce</h2>
-                        <p>This project focused on improving upon my previous ecommerce project that was only done with html and css and instead doing it with react this time around. This application will allow the user to browse through products and within the products page filter based on category and price.</p>
+                        <p className = {classes.infotext}>This project focused on improving upon my previous ecommerce project that was only done with html and css and instead doing it with react this time around. This application will allow the user to browse through products and within the products page filter based on category and price.</p>
                 </div>
                 <button className = {classes.projectButton}><a href ="https://github.com/darp-cs/ecommerce-anipop" target="_blank">Source Code</a></button>
                 </div>
@@ -44,7 +44,7 @@ const projects = () =>{
                 <img className= {classes.projectImg} src = "https://raw.githubusercontent.com/darp-cs/Images/main/reservation-app.png?raw=true" alt= "Hope Hackathon preview"/>
                 <div>
                         <h2 className= {classes.projectInfo}>Restaurant Reservation App</h2>
-                        <p>This project focused on bringing attention to a problem within our society, in this case the environment, and ways that we can prevent it. This project uses javascript and express for the back end requests.</p>
+                        <p className = {classes.infotext}>This project is an application that would allow an individual to make a reservation for a restaurant and demonstrates semantic HTML and CSS.</p>
                 </div>
                 <button className = {classes.projectButton}><a href ="https://github.com/darp-cs/ReservationApp" target="_blank">Source Code</a></button>
                 </div>
@@ -54,7 +54,7 @@ const projects = () =>{
                 <img className= {classes.projectImg} src = "https://raw.githubusercontent.com/darp-cs/Images/main/guessing-app.png?raw=true" alt= "Hope Hackathon preview"/>
                 <div>
                         <h2 className= {classes.projectInfo}>Guessing Game</h2>
-                        <p>This project focused on bringing attention to a problem within our society, in this case the environment, and ways that we can prevent it. This project uses javascript and express for the back end requests.</p>
+                        <p className = {classes.infotext}>This project focused on understanding how to use other packages that would add functionality such as using a mic.</p>
                 </div>
                 <button className = {classes.projectButton}><a href ="https://github.com/darp-cs/GuessingGame" target="_blank">Source Code</a></button>
                 </div>
@@ -64,7 +64,7 @@ const projects = () =>{
                 <img className= {classes.projectImg} src = "https://raw.githubusercontent.com/darp-cs/Images/main/pokemon-app.png?raw=true" alt= "Hope Hackathon preview"/>
                 <div>
                         <h2 className= {classes.projectInfo}>Pokemon Inquirer App</h2>
-                        <p>This project focused on bringing attention to a problem within our society, in this case the environment, and ways that we can prevent it. This project uses javascript and express for the back end requests.</p>
+                        <p className = {classes.infotext}>This project focused on creating an interactive terminal game and using information from a JSON file.</p>
                 </div>
                 <button className = {classes.projectButton}><a href ="https://github.com/darp-cs/PokemonInquirerApp" target="_blank">Source Code</a></button>
                 </div>
@@ -74,7 +74,7 @@ const projects = () =>{
                 <img className= {classes.projectImg} src = "https://raw.githubusercontent.com/darp-cs/Images/main/Weather-app.png?raw=true" alt= "Hope Hackathon preview"/>
                 <div>
                         <h2 className= {classes.projectInfo}>Weather App</h2>
-                        <p>This project focused on bringing attention to a problem within our society, in this case the environment, and ways that we can prevent it. This project uses javascript and express for the back end requests.</p>
+                        <p className = {classes.infotext}>This project focused on creating an application using an API that would allow a user to get the current weather within their city at the time.</p>
                 </div>
                 <button className = {classes.projectButton}><a href ="https://github.com/darp-cs/WeatherApp" target="_blank">Source Code</a></button>
                 </div>
@@ -85,7 +85,7 @@ const projects = () =>{
                 <img className= {classes.projectImg} src = "https://raw.githubusercontent.com/darp-cs/Images/main/todo-app.png" alt= "Hope Hackathon preview"/>
                 <div>
                         <h2 className= {classes.projectInfo}>To-Do List</h2>
-                        <p>This project focused on bringing attention to a problem within our society, in this case the environment, and ways that we can prevent it. This project uses javascript and express for the back end requests.</p>
+                        <p className = {classes.infotext}>This project focused on creating an application in which you could add and delete reminders or todos.</p>
                 </div>
                 <button className = {classes.projectButton}><a href ="https://github.com/darp-cs/ToDoApp" target="_blank">Source Code</a></button>
                 </div>
@@ -95,7 +95,7 @@ const projects = () =>{
                 <img className= {classes.projectImg} src = "https://raw.githubusercontent.com/darp-cs/Images/main/tictactoe.png?raw=true" alt= "Hope Hackathon preview"/>
                 <div>
                         <h2 className= {classes.projectInfo}>TicTacToe</h2>
-                        <p>This project focused on bringing attention to a problem within our society, in this case the environment, and ways that we can prevent it. This project uses javascript and express for the back end requests.</p>
+                        <p className = {classes.infotext}>This project is a small tic tac toe game that uses an algorithm to check if a player has won.</p>
                 </div>
                 <button className = {classes.projectButton}><a href ="https://github.com/darp-cs/TicTacToe" target="_blank">Source Code</a></button>
                 </div>
@@ -106,7 +106,7 @@ const projects = () =>{
                 <img className= {classes.projectImg} src = "https://raw.githubusercontent.com/darp-cs/Images/main/friends-app.png?raw=true" alt= "Hope Hackathon preview"/>
                 <div>
                         <h2 className= {classes.projectInfo}>Friends App</h2>
-                        <p>This project focused on bringing attention to a problem within our society, in this case the environment, and ways that we can prevent it. This project uses javascript and express for the back end requests.</p>
+                        <p className = {classes.infotext}>This project utilizes routing with react-router-dom along with MySQL to pull information about friends.</p>
                 </div>
                 <button className = {classes.projectButton}><a href ="https://github.com/darp-cs/FriendsApp" target="_blank">Source Code</a></button>
                 </div>
@@ -117,7 +117,7 @@ const projects = () =>{
                 <img className= {classes.projectImg} src = "https://raw.githubusercontent.com/darp-cs/Images/main/clocksoliatair-img.png?raw=true" alt= "Clock Solitaire"/>
                 <div>
                         <h2 className= {classes.projectInfo}>Clock Solitaire</h2>
-                        <p>This project was an assignment to code a game of clock soliatire, in Java, that would play on its own and show you the moves that were made along the way as well as the outcome.</p>
+                        <p className = {classes.infotext}>This project was an assignment to code a game of clock soliatire, in Java, that would play on its own and show you the moves that were made along the way as well as the outcome.</p>
                 </div>
                 <button className = {classes.projectButton}><a href ="https://github.com/darp-cs/Clock-Solitaire" target="_blank">Source Code</a></button>
                 </div>
@@ -126,7 +126,7 @@ const projects = () =>{
                 <img className= {classes.projectImg} src = "https://raw.githubusercontent.com/darp-cs/Images/main/A-star-img.png?raw=true" alt= "A Star"/>
                 <div>
                         <h2 className= {classes.projectInfo}>A Star</h2>
-                        <p>This project is an implementation of the A* algortihm, that in an advancement on dijkstra's algorithm using Java</p>
+                        <p className = {classes.infotext}>This project is an implementation of the A* algortihm, that in an advancement on dijkstra's algorithm using Java</p>
                 </div>
                 <button className = {classes.projectButton}><a href ="https://github.com/darp-cs/A-Star-Implementation" target="_blank">Source Code</a></button>
                 </div>
@@ -135,7 +135,7 @@ const projects = () =>{
                 <img className= {classes.projectImg} src = "https://raw.githubusercontent.com/darp-cs/Images/main/Queens-img.png?raw=true" alt= "8 Queens Algorithm"/>
                 <div>
                         <h2 className= {classes.projectInfo}>8 Queens</h2>
-                        <p>This project was written in Java for the purpose of getting a solution to placing 8 queens on a board so that they will not be in a line of sight of each other. The algorithm will continue to try and restart if the cost is too much until it finds a solution.</p>
+                        <p className = {classes.infotext}>This project was written in Java for the purpose of getting a solution to placing 8 queens on a board so that they will not be in a line of sight of each other. The algorithm will continue to try and restart if the cost is too much until it finds a solution.</p>
                 </div>
                 <button className = {classes.projectButton}><a href ="https://github.com/darp-cs/8-Queens" target="_blank">Source Code</a></button>
                 </div>
@@ -146,7 +146,7 @@ const projects = () =>{
                 <img className= {classes.projectImg} src = "https://raw.githubusercontent.com/darp-cs/Images/main/Floating%20point%20co-processor.png?raw=true" alt= "code output"/>
                 <div>
                         <h2 className= {classes.projectInfo}>Floating Point Co-Processor</h2>
-                        <p>This project is meant to be a simulation of what a floating point co-processor does when instructions are given to it in terms of the order of instructions using Python.</p>
+                        <p className = {classes.infotext}>This project is meant to be a simulation of what a floating point co-processor does when instructions are given to it in terms of the order of instructions using Python.</p>
                 </div>
                 <button className = {classes.projectButton}><a href ="https://github.com/darp-cs/Computer-Architecture.git" target="_blank">Source Code</a></button>
                 </div>
@@ -155,7 +155,7 @@ const projects = () =>{
                 <img className= {classes.projectImg} src = "https://raw.githubusercontent.com/darp-cs/Images/main/amber-detector-img.png?raw=true" alt= "code output"/>
                 <div>
                         <h2 className= {classes.projectInfo}>Amber Detection</h2>
-                        <p>This project was a group project in which we trained a neural network using fastai and 15,000+ images in order to correctly identify cars based on make and model. In addition we also added functionality to asses the color of the vehicle and also read the license plate if the image was a rear view image.</p>
+                        <p className = {classes.infotext}>This project was a group project in which we trained a neural network using fastai and 15,000+ images in order to correctly identify cars based on make and model. In addition we also added functionality to asses the color of the vehicle and also read the license plate if the image was a rear view image.</p>
                 </div>
                 <button className = {classes.projectButton}><a href ="https://github.com/darp-cs/Amber-Detector" target="_blank">Source Code</a></button>
                 </div>
@@ -164,7 +164,7 @@ const projects = () =>{
                 <img className= {classes.projectImg} src = "https://raw.githubusercontent.com/darp-cs/Images/main/snake-game-img.png?raw=true" alt= "code output"/>
                 <div>
                         <h2 className= {classes.projectInfo}>Snake Game</h2>
-                        <p>This project is a simple Python snake game that will run in the terminal.</p>
+                        <p className = {classes.infotext}>This project is a simple Python snake game that will run in the terminal.</p>
                 </div>
                 <button className = {classes.projectButton}><a href ="https://github.com/darp-cs/SnakeGame" target="_blank">Source Code</a></button>
                 </div>
@@ -175,7 +175,7 @@ const projects = () =>{
                 <img className= {classes.projectImg} src = "https://github.com/darp-cs/Images/blob/main/Dungeon-game-img.png?raw=true" alt= "code output"/>
                 <div>
                         <h2 className= {classes.projectInfo}>Dungeon Game</h2>
-                        <p>This project was a game written in C# and ran in unity. The game consists of levels, health system, damage system, AI, as well as player stats. The setting for the game is a dungeon and so far the only enemies are skeletons.</p>
+                        <p className = {classes.infotext}>This project was a game written in C# and ran in unity. The game consists of levels, health system, damage system, AI, as well as player stats. The setting for the game is a dungeon and so far the only enemies are skeletons.</p>
                 </div>
                 <button className = {classes.projectButton}><a href ="https://github.com/darp-cs/Dungeon-Game" target="_blank">Source Code</a></button>
                 </div>
